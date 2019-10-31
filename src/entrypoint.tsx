c62852943c168c121a7d0b61236ed7ef4ec8d83b
LE GOFF <dev.legoff@gmail.com>
2019-10-31 14:13:25 +0100

    init repo

---
+++ src/entrypoint.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+// App wrapped with redux Provider, store, etc.
+
+import { App } from './ui/App'
+
+render(<App />, document.getElementById('root'))
---
+++ src/ui/App.tsx
@@ -0,0 +1,13 @@
+
+import * as React from 'react'
+// routing, etc.
+
+export class App extends React.Component {
+    public render() {
+        return (
+            <div>
+                <title>Dashboard</title>
+            </div>
+        )
+    }
+}
