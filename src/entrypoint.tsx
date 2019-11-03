14ab046c50e5bd47c54bb9683d48fd560ec32d15
loiclegoff <dev.legoff@gmail.com>
2019-11-03 12:12:51 +0100

    refacto

--- src/entrypoint.tsx
+++ src/entrypoint.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { render } from 'react-dom'
-// App wrapped with redux Provider, store, etc.
+// App wrapped with redux Provider, store, etc. (if necessary)
 
 import { App } from './main/App'
 
